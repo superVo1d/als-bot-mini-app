7a267bde9b9beade55d6a278c778e712c167cce9
Grigory Mylnikov <supervoid18@gmail.com>
2024-09-07 16:04:46 +0300

    Добавлен манифест для Тона

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { type PropsWithChildren, useEffect, useMemo, useState } from "react";
+import { type PropsWithChildren, useEffect, useMemo } from "react";
 import {
   SDKProvider,
   useLaunchParams,
@@ -12,7 +12,7 @@ import {
   bindViewportCSSVars,
   useBackButton,
 } from "@telegram-apps/sdk-react";
-import { initSwipeBehavior, retrieveLaunchParams } from "@telegram-apps/sdk";
+import { initSwipeBehavior } from "@telegram-apps/sdk";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { AppRoot } from "@telegram-apps/telegram-ui";
 
