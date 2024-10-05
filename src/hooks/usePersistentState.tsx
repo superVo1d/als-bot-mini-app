a6c0f8c8c4f48189d08730747a5a9f9e5f87ad47
Grigory Mylnikov <supervoid18@gmail.com>
2024-10-05 12:12:56 +0300

    Сохранение в сессию

--- src/hooks/usePersistentState.tsx
+++ src/hooks/usePersistentState.tsx
@@ -5,7 +5,7 @@ function usePersistentState<T>(
   initialValue: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
   const getInitialValue = () => {
-    const storedValue = localStorage.getItem(key);
+    const storedValue = sessionStorage.getItem(key);
     if (storedValue !== null) {
       return eval(storedValue) as T;
     }
@@ -15,7 +15,7 @@ function usePersistentState<T>(
   const [value, setValue] = useState<T>(getInitialValue);
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    sessionStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
   return [value, setValue];
